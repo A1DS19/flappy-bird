a1242ff260fdb5a766a063b8a031653bf432e875
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-04-22 14:56:13 -0700

    state machine

--- include/stateMachine.hpp
+++ include/stateMachine.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <algorithm>
+#include <memory>
+#include <stack>
+#include <state.hpp>
+
+namespace Sonar {
+typedef std::unique_ptr<state::State> StateRef;
+
+class StateMachine {
+public:
+  StateMachine();
+  ~StateMachine();
+  void AddState(StateRef newState, bool isReplacing = true);
+  void RemoveState();
+  void ProcessStateChanges();
+  StateRef &GetActiveState();
+
+private:
+  std::stack<StateRef> _states;
+  StateRef _newState;
+  bool _isRemoving;
+  bool _isAdding;
+  bool _isReplacing;
+};
+} // namespace Sonar
